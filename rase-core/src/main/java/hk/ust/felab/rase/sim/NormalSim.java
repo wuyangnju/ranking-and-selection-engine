f26ad36289e25b9438eb6cc51391f8805ca427f5
Wu Yang <wuyangnju@gmail.com>
2013-07-25 23:25:24 +0800

    const delayed normal

--- rase-core/src/main/java/hk/ust/felab/rase/sim/NormalSim.java
+++ rase-core/src/main/java/hk/ust/felab/rase/sim/NormalSim.java
@@ -8,8 +8,23 @@ public class NormalSim implements Sim {
 
 	@Override
 	public double[] sim(double[] alt, double[] args, long[] seed) {
+		int delay;
+		if (args.length != 0) {
+			delay = (int) args[0];
+		} else {
+			delay = 0;
+		}
+
 		MRG32k3a mrg32k3a = new MRG32k3a();
 		mrg32k3a.setSeed(seed);
+
+		if (delay != 0) {
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		return new double[] { NormalGen.nextDouble(mrg32k3a, alt[1], alt[2]) };
 	}
 
